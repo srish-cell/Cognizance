2f8d85608254285edced9471478f957b64e9f83d
Srish <58882869+srish-cell@users.noreply.github.com>
2022-02-21 23:41:10 +0530

    Update Question-1.py

--- Task-4/Question-1.py
+++ Task-4/Question-1.py
@@ -1,4 +1,4 @@
-print("This program is to find the sum of two given numbers")
+#This program is to find the sum of two given numbers
 n1 = int(input("Enter First number: "))
 n2 = int(input("Enter Second number: "))
 sum = n1 + n2
