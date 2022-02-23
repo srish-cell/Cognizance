d8ae543903f3597b6f11660855e0968fa3363273
Srish <58882869+srish-cell@users.noreply.github.com>
2022-02-23 15:57:55 +0530

    Add files via upload

--- Task-4/Question-1.py
+++ Task-4/Question-1.py
@@ -1,4 +1,4 @@
-#This program is to find the sum of two given numbers
+print("This program is to find the sum of two given numbers")
 n1 = int(input("Enter First number: "))
 n2 = int(input("Enter Second number: "))
 sum = n1 + n2
