8596621f3bc085c919a0789f9e32c8f84222e40c
Srish <58882869+srish-cell@users.noreply.github.com>
2022-03-09 20:45:01 +0530

    Add files via upload

--- Task-8/Task-8 Q-2.py
+++ Task-8/Task-8 Q-2.py
@@ -1,6 +1,15 @@
 import numpy as np
-A=np.array([1, 0, 0, 0, 1, 0])
-B=np.array([0, 0, 1, 1, 0, 1])
+n=int(input("Enter the size of the array: "))
+A=np.array([])
+B=np.array([])
+print("Enter the values for Array A")
+for i in range (n):
+    print("A[",i,"] =\t")
+    A=np.append(A,int(input()))
+print("Enter the values for Array B")
+for i in range (n):
+    print("B[",i,"] =\t")
+    B=np.append(B,int(input()))
 print("First Array: \n",A)
 print("Second Array: \n",B)
 if (np.array_equal(A,B))==True:
